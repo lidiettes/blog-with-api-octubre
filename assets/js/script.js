5656914cbd2db12ff229e1bfe8baf5a94a9ed776
juanfco86 <juanfco86@gmail.com>
2022-10-08 20:01:35 +0200

    user API con post API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,37 @@
 //variables
 
-const intro = document.getElementById("main-title")
-const containerIntro = document.getElementById("container-intro") 
-const containerModal = document.getElementById("container-modal")
+const
+        intro = document.getElementById("main-title"),
+        containerIntro = document.getElementById("container-intro"),
+        containerModal = document.getElementById("container-modal");
 
 
 //functions
 
-function getPost() {
-    
+async function getPost() {
+    const url = "https://jsonplaceholder.typicode.com/users";
+    const response = await fetch(url);
+    const asyncData = await response.json();
+
+
     fetch("https://jsonplaceholder.typicode.com/posts")
 
         .then(response => response.json())
         .then(data => {
-            for (let i = 0; i < 8; i++) {
+            for (let i = 0; i < data.length; i++) {
                 let post = document.createElement("div");
                 post.innerHTML = `
                 <div class="shadow-lg p-3 mb-5 bg-body rounded w-70 main-container"  >
                     <div class="row row-cols-1 " >
                         <div id= "main-title" class="col" type="button" class="btn btn-primary" data-bs-toggle="modal"
                         data-bs-target="#post${i}">"${data[i].title}"
-                        <i class="fa-solid fa-pen-to-square" style="margin-left:20px;"></i>
-                        <i class="fa-solid fa-trash-can"></i> </div>
-                        
-                    
+                            <i class="fa-solid fa-pen-to-square" style="margin-left:20px;"></i>
+                            <i class="fa-solid fa-trash-can"></i>
+                        </div>
                     </div>
-                    
                 </div>
                 `
                 containerIntro.appendChild(post);
-
 
                 //modal
                 let modal = document.createElement("div");
@@ -43,18 +45,43 @@ function getPost() {
                         </div>
                         <div class="modal-body">
                             <div id="modal-title" > "${data[i].title}"</div> 
-                            <div id="modal-body"> "${data[i].body}" </div> 
-                            <div id="modal-user">"${data[i].userId}"</div>
+                            <div id="modal-body"> "${data[i].body}" </div>
                             <hr>
-                            <p>Comments</p></div>
+
+                            <p>User</p>
+                                <p>${asyncData[i].name}</p>
+                                <p>${asyncData[i].email}</p>
+
+                            <hr>
+
+                            <p>Comments</p>
+
+
+
+                        </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Load comments</button>
                         </div> </div> </div> </div> 
                 `
                 containerModal.appendChild(modal);
             }
-        })
+        });
+
 }
+
+
+// function getComments() {
+//     fetch('https://jsonplaceholder.typicode.com/posts/:id/comments')
+//         .then(response => response.json())
+//         .then(data => {
+
+//         })
+
+//}
+
 //steps
 
 getPost();
+// getComments();
+
+
