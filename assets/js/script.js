0e4ab50fa73776b7a0946dbc9e8639e09c2d9a76
lidiettes <hello@lidiettes.com>
2022-10-10 09:11:10 +0200

    try fix getUser

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
-//variables
-
 window.onload = getPost();
 
+//variables
 const intro = document.getElementById("main-title")
 const containerIntro = document.getElementById("container-intro") 
 const containerModal = document.getElementById("container-modal")
@@ -10,23 +9,7 @@ const containerModal = document.getElementById("container-modal")
 const userName = []; 
 const userId = [];
 
-
-//functions
-
-// function SaveUser(){
-//     fetch ("https://jsonplaceholder.typicode.com/users")
-//     .then (response => response.json())
-//     .then (data => {
-//         data.forEach (e => {
-//             userName.push(e.username)
-//             userId.push(e.id)
-//         })
-//     })
-// }
-
-
 function getPost() {
-    
     // async function getData(){
     //     const urlUsers = "https://jsonplaceholder.typicode.com/users";
     //     const urlComments = "https://jsonplaceholder.typicode.com/posts/:id/comments";
@@ -39,75 +22,53 @@ function getPost() {
     fetch("https://jsonplaceholder.typicode.com/posts")
         .then(response => response.json())
         .then(data => {
-            for (let i = 0; i < 8; i++) {
+            for (let i = 0; i < 15; i++) {
                 let post = document.createElement("div");
                 post.innerHTML = `
-                <div class="shadow-lg p-3 mb-5 bg-body rounded w-70 main-container"  >
+                <div class="shadow-lg p-2 mb-2 bg-body rounded w-70 main-container"  >
                     <div class="row row-cols-1 " >
-                        <div id= "main-title" class="col" type="button"  class="btn btn-primary" data-bs-toggle="modal"
-                        data-bs-target="#post${i}">"${data[i].title}"
-                        <i class="fa-solid fa-pen-to-square" style="margin-left:20px;"></i>
-                        <i class="fa-solid fa-trash-can"></i> </div>
-                        
-                    
+                        <div id= "main-title" class="col" class="row" type="button"  class="btn btn-primary" data-bs-toggle="modal"
+                        data-bs-target="#post${i}">${data[i].title}
+                            <i class="fa-solid fa-pen-to-square" style="margin-left:20px;"></i>
+                            <i class="fa-solid fa-trash-can"></i> </div>
                     </div>
-                    
                 </div>
                 `
                 containerIntro.appendChild(post);
 
 
                 //modal
-                let modal = document.createElement("div");
-                modal.innerHTML = `
-                    <div class="modal fade" id="post${i}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog">
-                                <div class="modal-content">
+            let modal = document.createElement("div");
+            modal.innerHTML = `
+                <div class="modal fade" id="post${i}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
                                     <div class="modal-header">
                                     <h5 class="modal-title" id="exampleModalLabel">Post</h5>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                     </div>
 
                                     <div class="modal-body">
-                                        <div id="modal-title" > "${data[i].title}"</div> 
+                                        <div id="modal-title" > ${data[i].title}</div> 
                                         <div id="modal-body"> "${data[i].body}" </div> 
-                                        <div id="modal-user" ><p> Names: </p> </div>
+                                        <div id="modal-user"><p> Names: </p></div>
                                         <hr>
-                                        <div id="container-comments" > <p class="p-comments">Comments</p> </div>
+                                        <div id="modal-comments" > <p class="p-comments">Comments</p> </div>
                                     </div>
 
                                     <div class="modal-footer" >
                                     <button id = "commentsButton" type="button" class="btn btn-primary" onclick="getComments()">Load comments</button>
                                     </div> 
-
-                                </div> 
                         </div> 
                     </div> 
+                </div> 
                 `
                 containerModal.appendChild(modal);
             }
         })
 }
 
-function getComments() {
-    fetch("https://jsonplaceholder.typicode.com/posts/1/comments")
-    .then(response => response.json())
-    .then(data => {
-        const containerComments = document.getElementById("container-comments")
-        for (let i = 0; i < 8; i++) {
-        let comments = document.createElement("p");
-        comments.innerHTML = `
-                            ${data[i].body}
-                            `
-        containerComments.appendChild(comments);
-        }
-        
-    })
-    
-}
-
 function getUsers() {
-
     fetch("https://jsonplaceholder.typicode.com/users/")
     .then(response => response.json())
     .then(data => {
@@ -121,8 +82,23 @@ function getUsers() {
                 containerName.appendChild(nameUser);
             // }
         }
-        
     })
-    
 }
+
+function getComments() {
+    fetch("https://jsonplaceholder.typicode.com/posts/1/comments")
+    .then(response => response.json())
+    .then(data => {
+        const containerComments = document.getElementById("modal-comments")
+            for (let i = 0; i < 8; i++) {
+            let comments = document.createElement("p");
+            comments.innerHTML = `
+                            ${data[i].email}
+                            `
+            containerComments.appendChild(comments);
+            }
+    })
+}
+
+
 
